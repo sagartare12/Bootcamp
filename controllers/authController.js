aea281bc05dc2ce0d3b2e60dd2ff5c8da9a958cd
sagartare <sagartare2021@gmail.com>
2022-08-19 01:15:42 +0530

    Log out feature added

--- controllers/authController.js
+++ controllers/authController.js
@@ -84,6 +84,18 @@ createSendToken(user,200,res);
 //       token
 //   })
 })
+
+
+//log out 
+exports.logout = (req , res)=>{
+  res.cookie('jwt','log out',{
+    expires : new Date(Date.now() + 10 * 1000),
+    httpOnly: false
+  })
+  res.status(200).json({status:'success'})
+}
+
+
 exports.protect = catchAsync(async (req, res, next) => {
   // 1) Getting token and check of it's there
   let token;
